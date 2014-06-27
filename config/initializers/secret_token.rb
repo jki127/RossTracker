e77feaa9d37d3aa00456349b1a8f1818250838b8
Jayson Isaac <jayson@challengepost.com>
2014-06-27 11:02:00 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RossTracker::Application.config.secret_key_base = '94154920333337470822753cbb966682e4dba420f9ca7399e8714bd3a7f662e6a3a47ce6dc9a91edab5cebf81f085efd96c47c32ecc0bee13163792765817d00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RossTracker::Application.config.session_store :cookie_store, key: '_RossTracker_session'
