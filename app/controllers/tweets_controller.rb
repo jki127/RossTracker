4788e38c03c1594977a8bb6d8f8baf826f8b9b56
Alex <awheeler@bu.edu>
2014-06-27 13:17:02 -0400

    removes stuff

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,24 +9,8 @@ class TweetsController < ApplicationController
       config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
     end
 
-    @client.update(params[:tweet][:text] + " #TeamRoss")
+    @client.update(params[:tweet][:text] + "#rossisthecoolest yea")
     redirect_to root_path
   end
 
 end
-
-
-
-
-
-@client.search("myhashtagalex")
-end
-
-
-
-@client = Twitter::REST::Client.new do |config|
-      config.consumer_key = "WySk1MeJwuJoOFj5ApGTd0FDD"
-      config.consumer_secret = "9PUKpwmod3YrwCSyKEOGO8eJR3LclSz2EO1iaWRn5kA9CLpWh8"
-      config.access_token = "2591580042-MbKEGMcjbAXqDXLmWQyrkzNYhI4AmMqn4Hk4Iju"
-      config.access_token_secret = "SXZCvclTFHNbqBphibRkI4Fjay5ZdnYaWp5GJzxm19dI6"
-    end
